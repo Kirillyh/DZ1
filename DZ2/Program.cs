591620788b5e4acbf552a10b437de8cfd3e63a42
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-11-30 10:18:58 +0300

    Добавили 2 задачу

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -2,3 +2,41 @@
 // 3 -> Среда
 // 5 -> Пятница
 
+Console.WriteLine("Введите номер дня:"); 
+
+string a = Console.ReadLine();   // считываем с консоли переменную a
+
+int b =  Convert.ToInt32(a);     //создаем переменную конвентируем переменную a
+
+if (b == 1)
+{
+Console.WriteLine("Понедельник");
+}
+if (b==2)
+{
+    Console.WriteLine("Вторник");
+}
+if (b==3)
+{
+    Console.WriteLine("Среда");
+}
+if (b==4)
+{
+    Console.WriteLine("Четверг");
+}
+if (b==5)
+{
+    Console.WriteLine("Пятница");
+}
+if (b==6)
+{
+    Console.WriteLine("Суббота");
+}
+if (b==7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if (b>7||b<1)                //Условие мы попадаем в это условие Console.WriteLine("Какой сегодня день:");
+{
+    Console.WriteLine("Какой сегодня день:");
+} 

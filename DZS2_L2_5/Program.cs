764546ef57081a4abe6f24d22b5f7c5b860a2da1
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-12-03 20:34:23 +0300

    Добавили 16 задачу

---
+++ DZS2_L2_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 16: Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+
+Console.WriteLine("Введите первое число:");
+int numbera = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int numberb = Convert.ToInt32(Console.ReadLine());
+
+if (numbera * numbera == numberb || numberb * numberb == numbera)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}

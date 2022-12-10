ef1c17b169d2c2067a876842b216d30537394e6b
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-12-10 16:38:51 +0300

    Сделали задачу 26

---
+++ DZS4_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int ReadInt(string message)
+{
+   Console.WriteLine(message);
+   return Convert.ToInt32(Console.ReadLine()); 
+}
+//Возвращает сумму от 1 до number
+int GetSumToNumber(int number)
+{
+    int count= 0;                 //Сколько раз число поделиться на 10
+
+    while (number !=0) //number не равен 0
+    {
+        number /=10; // number = number / 10
+        count++;
+    }
+    return count;
+}
+int number = ReadInt("Введите число:  ");  //обрашаемся к методу
+int result = GetSumToNumber(number);      
+Console.WriteLine(result);

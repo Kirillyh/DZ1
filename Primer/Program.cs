ce6cdd591f5814d79fabb12e1e1662081931e1ea
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-12-11 10:21:18 +0300

    Сделали задачу 25

--- Primer/Program.cs
+++ Primer/Program.cs
@@ -1,3 +1,19 @@
-﻿int a = 12345;
-int b = a / 1000%10;
-Console.WriteLine(b);
+﻿// int a = 12345;
+// int b = a / 1000%10;
+// Console.WriteLine(b);
+int ReadInt(string message)
+{
+   Console.WriteLine(message);
+   return Convert.ToInt32(Console.ReadLine()); 
+}
+int A = ReadInt("Введите число A:");
+int B = ReadInt("Введите число B:");
+
+int count = 1;
+for (int i = 1; i <= B; i++ )
+{
+count = count * A;
+}
+Console.WriteLine(count);               // return count;
+
+// Console.WriteLine(count);

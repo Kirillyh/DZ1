eb265a981a8ce369fc95f82c2c52a468e51f8469
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-12-19 12:02:18 +0300

    Добавили задачу 45 Создать копию массива

---
+++ DZS6_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] array = {1,2,3,4,5};
+int[] coparray = new int [array.Length];
+for(int i =0; i < coparray.Length; i++)
+{
+    coparray[i] = array[i];
+}
+Console.WriteLine(string.Join("," , coparray ));

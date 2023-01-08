bdd70a57fa55b616c1556e94ffbc219b7f9c047e
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2023-01-08 13:29:15 +0300

    Добавили задачу 67

---
+++ DZS9_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число :");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int Line (int sum)
+{
+    if (sum == 0) return 0;
+    else 
+    {
+        sum = sum%10 + Line(sum/10);
+    }
+    return sum;
+}
+Console.WriteLine(Line(num));
